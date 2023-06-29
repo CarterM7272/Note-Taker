4ae9c6c06679b162363be1cef51b853b9f8b1ff7
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-06-28 21:18:41 -0500

    worked on server a little bit

--- server.js
+++ server.js
@@ -1,38 +1,44 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 
-
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
-appendFile.listen(PORT, (err_ => {
-  if (err) console.log(err);
-  console.log(`App listening on Post: ${PORT}`)
-}));
 
 
-app.get('/', (req, res) => res.send(''))
-
-app.get('/send', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/'))
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 )
 
 app.get('/api/notes', (req, res) => {
 
 })
 
-if (title && text) {
-  const newNote = {
-    title,
-    text
+app.post('/api/notes', (req,res) => {
+
+  if (title && text) {
+    const noteRequirements = {
+      title,
+      text
+    }
   }
 
-  
-  const noteReview = JSON.stringify(newNote);
+  fs.readFile('./db/db.json', 'utf8', (err, data)  => {
+    if (err) {
+      console.error(err);
+    } else {
+      const newNote = JSON.parse(data); 
+    }
+  })
 
+
+
+  
 
   fs.writeFile(`./db/db.json`, noteReview, (err) => {
     
@@ -45,5 +51,20 @@ if (title && text) {
 
 
 
+})
 
-}
+app.delete('/api/notes', (req,res) => {
+  
+})
+
+const { title, text } = req.body
+
+
+  app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    console.log(`App listening on Post: ${PORT}`);
+  });
+
+
+
+
