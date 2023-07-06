21b93f12e3224d21bb9e9510d2d6047e65f98b50
cmarkham7272@gmail.com <cmarkham7272@gmail.com>
2023-07-06 16:56:46 -0500

    still trying to append data to screen, still getting 404 status code from /api/notes.

--- server.js
+++ server.js
@@ -7,22 +7,32 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-
+// Set the MIME type for CSS files
+app.use('/css', express.static(path.join(__dirname, 'public/css'), { 
+  setHeaders: (res) => {
+    res.setHeader('Content-Type', 'text/css');
+  }
+}));
 
 app.get('/', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 )
 
-app.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-app.post('/api/notes', (req,res) => {
+app.post('/api/notes', async (req, res) => {
 
-  if (title && text) {
+  const { title, text } = req.body
+
+
+  if (!title || !text) {
+    return res.status(500).json(`Please enter in a title or text to add note`)
+  }
+
     const noteRequirements = {
       title,
       text
@@ -30,36 +40,44 @@ app.post('/api/notes', (req,res) => {
     
     fs.readFile('./db/db.json', 'utf8', (err, data)  => {
       if (err) {
-        console.error(err);
+        console.error("err");
       } else {
         const newNote = JSON.parse(data);
-        
+
         newNote.push(noteRequirements)
 
-        fs.writeFile(`./db/db.json`, newNote, (err) => {
+        fs.writeFile(`./db/db.json`, JSON.stringify(newNote), (writeErr) => {
+          if (writeErr) {
+            console.err(writeErr)
+          } else {
+            const response = {
+              status: 'success',
+              body: newNote,
+            };
+            console.log(response);
+            res.status(201).json(response)
+          }
+        }
+          )
+        }})
       
-        })
-
-
-
-
-
-      }
-    })
+    
+  
   
 
-  
-    const response = {
-      status: 'success',
-      body: newNote,
-    }
-  }})
-
-app.delete('/api/notes', (req,res) => {
-  
 })
 
-// const { title, text } = req.body
+
+
+
+
+
+app.delete('/api/notes:title', (req,res) => {
+  const noteId = req.params.title;
+  res.status(200).json({message: 'Delete Successful'})
+})
+
+
 
 
   app.listen(PORT, (err) => {
